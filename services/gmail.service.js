42b08650d60de01ba5fa3af1713b642d8f4b6ea0
Martin Diaz <144454009+martindiaz14@users.noreply.github.com>
2026-03-13 13:40:08 -0300

    Update gmail.service.js

--- services/gmail.service.js
+++ services/gmail.service.js
@@ -11,7 +11,8 @@ const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000';
 const transporter = nodemailer.createTransport({
     service: 'gmail', 
     auth: {
-
+        user: process.env.EMAIL_USER || 'martindiazb14@gmail.com', 
+        pass: process.env.EMAIL_PASS || 
     }
 });
 
